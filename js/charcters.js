184620dcf954b382946e0292d2eb9bd595e5a72f
cleyton27 <cleyton27mb@gmail.com>
2019-02-09 10:01:15 -0300

    Implementanto a função showCharacters() que traz as informações dos personagens

--- js/charcters.js
+++ js/charcters.js
@@ -1,37 +1,15 @@
-let btnC = document.getElementById("cha")
+let btnC = document.getElementById('cha');
 
-btnC.addEventListener("click", function(){
-  btnC.value ="people"
-  getJesonData(btnC.value, showDatasCharacters)
+btnC.addEventListener("click",function(){
+  btnC.value = "people";
+  getJesonData(btnC.value, showDataCharacters)
 })
 
-
-function showDatasCharacters(datas){
+function showDataCharacters(datas){
 datas.forEach(x=>{
-   h2 = document.createElement('h2');
-   h3 = document.createElement('h3');
-   p = document.createElement('p');
-   //btnCharacter = document.createElement('button');
-
-   btnPlanets = document.createElement('button');
-   btnVehicles = document.createElement('button');
-
-
+  let h2 = document.createElement('h2');
       h2.textContent = x.name;
-      p.textContent = x.opening_crawl;
-      h3.textContent = `Director `+x.director+` and Producer `+x.producer;
-      btnCharacter.textContent = `Characters`;
-      btnPlanets.textContent = `Planets`;
-      btnVehicles.textContent = `Vehicles`
 
-
-
-      //console.log(title);
-
-      //console.log(typeof(x));
-
-      //console.log(btnCharacter);
-      //movie.appendChild(h2)
       movie.appendChild(h2)
 });
 
